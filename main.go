49f838954d102b03978232440887ceb922208407
m0090-dev <maru1015928@gmail.com>
2026-01-10 02:35:01 +0900

    Update

--- main.go
+++ main.go
@@ -8,7 +8,8 @@ import (
 	"github.com/wailsapp/wails/v2"
 	"github.com/wailsapp/wails/v2/pkg/options"
 	"github.com/wailsapp/wails/v2/pkg/options/assetserver"
-    "github.com/wailsapp/wails/v2/pkg/options/linux"
+        "github.com/wailsapp/wails/v2/pkg/options/linux"
+        "github.com/wailsapp/wails/v2/pkg/options/windows"
 )
 
 //go:embed all:frontend/dist
